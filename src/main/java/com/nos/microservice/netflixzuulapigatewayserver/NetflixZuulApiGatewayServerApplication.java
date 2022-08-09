fc425ae1fde832796bddb90de2b64cba477af5b2
Shekhar Saha <saha.shekhar704@gmail.com>
2022-08-09 22:26:16 +0530

    routes update

--- src/main/java/com/nos/microservice/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
+++ src/main/java/com/nos/microservice/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-
-import brave.sampler.Sampler;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -17,10 +14,9 @@ public class NetflixZuulApiGatewayServerApplication {
 		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
 	}
 	
-	@Bean
-	public Sampler defaultSampler() {
-		
-		return Sampler.ALWAYS_SAMPLE;
-	}
-
+	/*
+	 * @Bean public Sampler defaultSampler() {
+	 * 
+	 * return Sampler.ALWAYS_SAMPLE; }
+	 */
 }
